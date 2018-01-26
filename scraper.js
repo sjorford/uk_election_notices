3a5ec7b24aa7342f5c826e7f57f099e157e8d9cf
sjorford <sjorford@gmail.com>
2018-01-26 17:14:24 +0000

    test

--- scraper.js
+++ scraper.js
@@ -4,11 +4,14 @@ var cheerio = require("cheerio");
 var request = require("request");
 var sqlite3 = require("sqlite3").verbose();
 
+// https://stackoverflow.com/questions/10011011/using-node-js-how-do-i-read-a-json-object-into-server-memory
+var pages = require('pages.json');
+
 function initDatabase(callback) {
 	// Set up sqlite database.
 	var db = new sqlite3.Database("data.sqlite");
 	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
+		db.run("CREATE TABLE IF NOT EXISTS pages (name TEXT, url TEXT, selector TEXT, contents TEXT)");
 		callback(db);
 	});
 }
@@ -20,7 +23,7 @@ function updateRow(db, value) {
 	statement.finalize();
 }
 
-function readRows(db) {
+function readRows(db, name) {
 	// Read some data.
 	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
 		console.log(row.id + ": " + row.name);
@@ -40,20 +43,56 @@ function fetchPage(url, callback) {
 }
 
 function run(db) {
-	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
+	
+	// Get list from pages.json
+	// For each page
+	// 		Download page
+	// 		Get selector
+	// 		Get full trimmed text
+	//		if not in database
+	//			insert
+	//		else
+	//			compare
+	//			if difference
+	//				add to differences table
+	//			update
+	// If differences
+	//		email differences
+	
+	console.log(pages);
+	console.log(pages.each);
+	
+	for (page in pages) {
+		
+		console.log(page);
+		
+		/*
+		// Use request to read in pages.
+		fetchPage(page.url, function (body) {
+			
+			// Use cheerio to find things in the page with css selectors.
+			var $ = cheerio.load(body);
 
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
+			var contents = $(page.selector).text().fullTrim();
+			
+			
+			updateRow
+			
+			each(function () {
+				var value = $(this).text().trim();
+				updateRow(db, value);
+			});
+
+			readRows(db);
+
+			db.close();
 		});
-
-		readRows(db);
-
-		db.close();
-	});
+		*/
+		
+		
+		
+	}
+	
 }
 
 initDatabase(run);
