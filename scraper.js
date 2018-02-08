8aef18e5316cd41f071914084a24c437864d9b2e
sjorford <sjorford@gmail.com>
2018-02-08 11:49:26 +0000

    hacking

--- scraper.js
+++ scraper.js
@@ -5,41 +5,20 @@ var sqlite3 = require("sqlite3").verbose();
 // https://stackoverflow.com/questions/10011011/using-node-js-how-do-i-read-a-json-object-into-server-memory
 var pages = require('./pages.json');
 
+var db;
+var i;
+
 function initDatabase(callback) {
-	// Set up sqlite database.
+	
+	// Set up sqlite database
 	var db = new sqlite3.Database("data.sqlite");
 	db.serialize(function() {
 		db.run("CREATE TABLE IF NOT EXISTS pages (name TEXT, url TEXT, selector TEXT, contents TEXT)");
 		callback(db);
 	});
-}
-
-function updateRow(db, value) {
-	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
-	statement.finalize();
-}
-
-function readRows(db, name) {
-	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
-	});
-}
-
-function fetchPage(url, callback) {
-	request(url, function (error, response, body) {
-		if (error) {
-			console.log("Error requesting page: " + error);
-			return;
-		}
-		callback(body);
-	});
-}
-
-function run(db) {
 	
+}
+
 	// 		Get selector
 	// 		Get full trimmed text
 	//		if not in database
@@ -52,65 +31,81 @@ function run(db) {
 	// If differences
 	//		email differences
 	
-	// Loop through pages
-	var i = 0;
+function run() {
+	
+	// Get first page
+	i = 0;
 	nextPage();
-		
-	function nextPage() {
-		
-		if (i >= pages.length) {
-			db.close();
-			console.log('processed ' + (pages.length + 1) + ' pages, finished');
+	
+}
+
+function nextPage() {
+	
+	// Finish if no more pages
+	if (i >= pages.length) {
+		db.close();
+		console.log('processed ' + pages.length + ' pages, finished');
+		return;
+	}
+	
+	// Fetch the page
+	console.log(i, pages[i]);
+	request(pages[i].url, function (error, response, body) {
+		if (error) {
+			console.log("Error requesting page: " + error);
 			return;
 		}
-		
-		// Fetch the next page
-		console.log(i, pages[i]);
-		fetchPage(pages[i].url, processfetchedPage);
-		
-	}
+		processfetchedPage(body);
+	});
+	
+}
 
-	function processfetchedPage(body) {
-		
-		// Get selected text
-		var $ = cheerio.load(body);
-		var target = $(pages[i].selector);
-		if (target.length == 0) {
-			console.error(i, 'selector not found');
-		} else if (target.length == 0) {
-			console.error(i, target.length + ' instances of selector found');
-		} else {
-			pages[i].contents = fullTrim(target.text());
-			console.log(i, pages[i].contents.substr(0, 100));
-		}
-		
-		readRow();
-		
-		/*
-		updateRow
-		
-		each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
-		});
-		
-		readRows(db);
-		
-		*/
-		
+function processfetchedPage(body) {
+	
+	// Get selected text
+	var $ = cheerio.load(body);
+	var target = $(pages[i].selector);
+	if (target.length == 0) {
+		console.error(i, 'selector not found');
+	} else if (target.length == 0) {
+		console.error(i, target.length + ' instances of selector found');
+	} else {
+		pages[i].contents = fullTrim(target.text());
+		console.log(i, pages[i].contents.substr(0, 100));
+	}
+	
+	// Read the row for this page
+	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
+		console.log(err, row);
+		//console.log(row.id + ": " + row.name);
+			
 		i++;
 		nextPage();
 		
-	}
+	});
 	
-	function readRow(text) {
-		
-		
-		
-		
-		
-	}
+	
+	
+	
+	/*
+	updateRow
+	
+	each(function () {
+		var value = $(this).text().trim();
+		updateRow(db, value);
+	});
+	
+	readRows(db);
+	
+	*/
+	
+}
 
+function updateRow(db, value) {
+	// Insert some data.
+	var statement = db.prepare("INSERT INTO data VALUES (?)");
+	statement.run(value);
+	statement.finalize();
 }
 
 function fullTrim(string) {
